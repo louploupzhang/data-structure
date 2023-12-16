d9af6c1d396ea644bb855bd9f995441a4510f90e
louploupzhang <zhang.sy@hotmail.com>
2023-12-16 00:36:04 -0500

    Recursion: optimized bubble sort

--- src/main/java/recursion/BubbleSort.java
+++ src/main/java/recursion/BubbleSort.java
@@ -10,14 +10,16 @@ public class BubbleSort {
     //j: right bound of unsorted area
     private static void bubble(int[] a, int j) {
         if (j == 0) return;
+        int x=0; //flag to isolate unordered & ordered parts
         for (int i = 0; i < j; i++) {
             if (a[i] > a[i + 1]) {
                 int t = a[i];
                 a[i] = a[i + 1];
                 a[i + 1] = t;
+                x = i;
             }
         }
-        bubble(a, j - 1);
+        bubble(a, x);
     }
 
     public static void main(String[] args) {
