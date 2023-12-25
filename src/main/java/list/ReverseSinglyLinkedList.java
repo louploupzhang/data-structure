de44aa653d3e593ebc7945b04737aab492318723
louploupzhang <zhang.sy@hotmail.com>
2023-12-24 23:37:52 -0500

    ReverseSinglyLinkedList: move nodes

--- src/main/java/list/ReverseSinglyLinkedList.java
+++ src/main/java/list/ReverseSinglyLinkedList.java
@@ -19,7 +19,29 @@ class ListNode {
 }
 
 public class ReverseSinglyLinkedList {
-    public ListNode reverseList(ListNode o1) {
+    static class List {
+        ListNode head;
+
+        public List(ListNode head) {
+            this.head = head;
+        }
+
+        public void addFirst(ListNode first) {
+            first.next = head;
+            head = first;
+        }
+
+        public ListNode removeFirst() {
+            ListNode first = head;
+            if (first != null) {
+                head = first.next;
+            }
+            return first;
+        }
+    }
+
+    //Method 1: create new list and copy values
+    public ListNode reverseList1(ListNode o1) {
         ListNode n1 = null;
         ListNode p = o1;
         while (p != null) {
@@ -29,6 +51,20 @@ public class ReverseSinglyLinkedList {
         return n1;
     }
 
+    //Method 2: move elements
+    public ListNode reverseList2(ListNode head) {
+        List list1 = new List(head);
+        List list2 = new List(null);
+        while (true){
+            ListNode first = list1.removeFirst();
+            if(first==null){
+                break;
+            }
+            list2.addFirst(first);
+        }
+        return list2.head;
+    }
+
     public static void main(String[] args) {
         ListNode o5 = new ListNode(5, null);
         ListNode o4 = new ListNode(4, o5);
@@ -36,7 +72,8 @@ public class ReverseSinglyLinkedList {
         ListNode o2 = new ListNode(2, o3);
         ListNode o1 = new ListNode(1, o2);
         System.out.println(o1);
-        ListNode n1 = new ReverseSinglyLinkedList().reverseList(o1);
+//        ListNode n1 = new ReverseSinglyLinkedList().reverseList(o1);
+        ListNode n1 = new ReverseSinglyLinkedList().reverseList2(o1);
         System.out.println(n1);
     }
 }
