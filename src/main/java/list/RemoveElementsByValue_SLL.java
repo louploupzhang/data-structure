42c35562420833e1ac121bd43e8dc334c2746d38
louploupzhang <zhang.sy@hotmail.com>
2023-12-27 23:28:54 -0500

    SinglyLinkedList: RemoveElementsByValue - Recursion

--- src/main/java/list/RemoveElementsByValue_SLL.java
+++ src/main/java/list/RemoveElementsByValue_SLL.java
@@ -5,33 +5,48 @@ public class RemoveElementsByValue_SLL {
     public ListNode removeElements(ListNode head, int val) {
         ListNode s = new ListNode(-1, head); //Sentinel
         ListNode p1 = s;
-        ListNode p2 = s.next;
-        while (p2 != null) {
+        //ListNode p2 = p1.next;
+        ListNode p2;
+        while ((p2 = p1.next) != null) {
             if (p2.val == val) {
-                //Delete node, move p2 forward
+                //Delete node
                 p1.next = p2.next;
-                p2 = p2.next;
             } else {
                 //Move pointers forward
                 p1 = p1.next;
-                p2 = p2.next;
             }
+            //Move p2 forward
+//            p2 = p1.next;
         }
         return s.next; //Sentinel's next is head
     }
 
+    //Recursion
+    public ListNode removeElements2(ListNode p, int val) {
+        if (p == null) {
+            return null;
+        }
+        if (p.val == val) {
+            return removeElements2(p.next, val);
+        } else {
+            //Make a jump when deletion is not needed
+            p.next = removeElements2(p.next, val);
+            return p;
+        }
+    }
+
     public static void main(String[] args) {
-//        ListNode o5 = new ListNode(6, null);
-//        ListNode o4 = new ListNode(3, o5);
-//        ListNode o3 = new ListNode(6, o4);
-//        ListNode o2 = new ListNode(2, o3);
-//        ListNode head = new ListNode(1, o2);
-        ListNode o5 = new ListNode(7, null);
-        ListNode o4 = new ListNode(7, o5);
-        ListNode o3 = new ListNode(7, o4);
-        ListNode o2 = new ListNode(7, o3);
-        ListNode head = new ListNode(7, o2);
+        ListNode o5 = new ListNode(6, null);
+        ListNode o4 = new ListNode(3, o5);
+        ListNode o3 = new ListNode(6, o4);
+        ListNode o2 = new ListNode(2, o3);
+        ListNode head = new ListNode(1, o2);
+//        ListNode o5 = new ListNode(7, null);
+//        ListNode o4 = new ListNode(7, o5);
+//        ListNode o3 = new ListNode(7, o4);
+//        ListNode o2 = new ListNode(7, o3);
+//        ListNode head = new ListNode(7, o2);
         System.out.println(head);
-        System.out.println(new RemoveElementsByValue_SLL().removeElements(head, 7));
+        System.out.println(new RemoveElementsByValue_SLL().removeElements2(head, 6));
     }
 }
